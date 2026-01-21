d9bd9aecbf5a5e04343775678044fcd486865fb5
Elie Nassar <nassarelie98@gmail.com>
2026-01-21 23:21:51 +0100

    add color to cells

--- Cell.java
+++ Cell.java
@@ -2,7 +2,8 @@ import javax.swing.JPanel;
 import java.awt.*;
 
 public class Cell extends JPanel{
-    private boolean alive;
+    private boolean alive; 
+    private float color;
 
     public Cell() {
         this(20);
@@ -10,25 +11,45 @@ public class Cell extends JPanel{
     public Cell(int size) {
         this.alive = false;
         setPreferredSize(new Dimension(size, size));
-        setBackground(Color.WHITE);
+        setColor(0);
         setOpaque(true);
     }
 
     public void live() {
         this.alive = true;
-        setBackground(Color.BLACK);
+        setColor(1);
     }
 
     public void kill() {
         this.alive = false;
-        setBackground(Color.WHITE);
+        setColor(0);
     }
 
     public void toggle() {
         this.alive = !alive;
-        setBackground(this.alive ? Color.BLACK : Color.WHITE);
+        setColor(this.alive ? 1 : 0);
     }
 
+    private static final int[][] INFERNO_PALETTE = {
+        {0, 0, 4}, {31, 12, 72}, {85, 15, 109}, {136, 34, 106}, {186, 54, 85}, {227, 89, 51}, {249, 140, 10}, {252, 195, 50}, {240, 249, 33}
+    };
+
+
+    public void setColor(float color) {
+        float scaled = color * (INFERNO_PALETTE.length - 1);
+        int index = (int) scaled;
+        float t = scaled - index;
+
+        int[] c0 = INFERNO_PALETTE[index];
+        int[] c1 = INFERNO_PALETTE[Math.min(index + 1, INFERNO_PALETTE.length - 1)];
+
+        int r = (int) (c0[0] + t * (c1[0] - c0[0]));
+        int g = (int) (c0[1] + t * (c1[1] - c0[1]));
+        int b = (int) (c0[2] + t * (c1[2] - c0[2]));
+
+        setBackground(new Color(r, g, b));
+    }
+    public float getColor() {return color;}
     public boolean isAlive() {return alive;}
 
 }
